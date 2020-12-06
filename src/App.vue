baec4a03c39b45c82ee6a01c37dfa27dcff9830c
ed <edahg.g@gmail.com>
2020-12-06 02:51:09 -0500

    Seccion Noticias

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-  </div>
+    <div class="container-fluid">      
+          <news-api></news-api>
+        </div>
+      </div>
+    
 </template>
 
 <script>
+import NewsApi from "./components/NewsApi.vue";
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    NewsApi,
+  },
+  data() {
+    return {
+      title: "Vivo dentro de VUE",
+    };
+  },
+};
 </script>
 
---
+++ src/components/NewsApi.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+      <div class="col-lg-6 col-xs-12" v-for="article in news" :key="article">
+        <div class="d-flex justify-content-center align-items-center">
+          <div class="p-3">
+            <img :src="article.image" alt="Article Name" height="200px" width = "200px">
+          </div>
+
+
+          <div class="p-2">
+              <h2>{{ article.title }}</h2>
+                <hr>
+            <p>
+              {{ article.description }}
+            </p>
+          </div>
+        </div>
+        <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+          <a :href="article.url" class="btn btn-outline-info" >Info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NewsApi",
+  data() {
+    return {
+      news: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://gnews.io/api/v4/top-headlines?&topic=science&max=4&lang=es&token=6ace8181fd09ff2352f98ac3e6a80f92"
+      )
+      .then((response) => {
+        this.news = response.data.articles;
+        console.log(this.news[0].image);
+      });
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
